9010d869973ef99d57d6d189a9dfe25402d90195
Artem Stankevich <artem.stankevich@redcollar.ru>
2024-12-08 13:37:54 +0300

    fix: fix dependancies

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/shared/styles')],
   },
